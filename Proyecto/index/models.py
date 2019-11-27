35d95ee014857b92d77c523e99562bc2dfa86ea9
Matías Ortega <mortegalopez635@gmail.com>
2019-11-27 00:25:36 -0300

    Post - post detail completo
    c:

--- Proyecto/index/models.py
+++ Proyecto/index/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class User(models.Model):
 
@@ -6,11 +7,22 @@ class User(models.Model):
     username = models.CharField(max_length=30)
     lastname = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
+    
+    def guardar(self):
+        self.save()
 
     def name(self):
         return self.name
 
-    def guardar(self):
+class Post(models.Model):
+
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    img = models.ImageField(upload_to='img')
+
+    def publish(self):
         self.save()
 
-
+    def __str__(self):
+        return self.title
